564c26ec6bac6910d9ccb45d3a4b2077d32cb8eb
Oliver Erxleben <oliver.erxleben@me.com>
2022-07-22 17:55:02 +0200

    Conf/project setup (#1)
    * initial commit
    
    * added blank react app/removed unused applications
    
    * added CODEOWNERS file

---
+++ apps/squirrel-news-app/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'squirrel-news-app',
+  webDir: '../../dist/apps/squirrel-news-app',
+  bundledWebRuntime: false,
+};
+
+export default config;
---
+++ apps/squirrel-news-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'squirrel-news-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/squirrel-news-app',
+};
